1a143d2489e2d35c1ad16c0292d1dc027a6458bf
SlingMaster <alexdovby@gmail.com>
2021-07-18 20:53:57 +0300

    bugfix time.ino

--- Firmware/time.ino
+++ Firmware/time.ino
@@ -1,15 +1,15 @@
 #ifdef USE_NTP
 
 #define RESOLVE_INTERVAL      (INTERNET_CHECK_PERIOD * 1000UL)            // интервал проверки подключения к интеренету в миллисекундах (INTERNET_CHECK_PERIOD секунд)
-// при старте ESP пытается получить точное время от сервера времени в интрнете
-// эта попытка длится RESOLVE_TIMEOUT
-// если при этом отсутствует подключение к интернету (но есть WiFi подключение),
-// модуль будет подвисать на RESOLVE_TIMEOUT каждое срабатывание таймера, т.е., 1.5 секунды
-// чтобы избежать этого, будем пытаться узнать состояние подключения 1 раз в RESOLVE_INTERVAL
-// попытки будут продолжаться до первой успешной синхронизации времени
-// до этого момента функции будильника работать не будут (или их можно ввести через USE_MANUAL_TIME_SETTING)
-// интервал последующих синхронизаций времени определяён в NTP_INTERVAL (30-60 минут)
-// при ошибках повторной синхронизации времени функции будильника отключаться не будут
+//                                                                           при старте ESP пытается получить точное время от сервера времени в интрнете
+//                                                                           эта попытка длится RESOLVE_TIMEOUT
+//                                                                           если при этом отсутствует подключение к интернету (но есть WiFi подключение),
+//                                                                           модуль будет подвисать на RESOLVE_TIMEOUT каждое срабатывание таймера, т.е., 1.5 секунды
+//                                                                           чтобы избежать этого, будем пытаться узнать состояние подключения 1 раз в RESOLVE_INTERVAL
+//                                                                           попытки будут продолжаться до первой успешной синхронизации времени
+//                                                                           до этого момента функции будильника работать не будут (или их можно ввести через USE_MANUAL_TIME_SETTING)
+//                                                                           интервал последующих синхронизаций времени определяён в NTP_INTERVAL (30-60 минут)
+//                                                                           при ошибках повторной синхронизации времени функции будильника отключаться не будут
 #define RESOLVE_TIMEOUT       (1500UL)                                    // таймаут ожидания подключения к интернету в миллисекундах (1,5 секунды)
 //uint64_t lastResolveTryMoment = 0xFFFFFFFFUL;
 IPAddress ntpServerIp = {0, 0, 0, 0};
@@ -18,12 +18,16 @@ IPAddress ntpServerIp = {0, 0, 0, 0};
 
 #if defined(USE_NTP) || defined(USE_MANUAL_TIME_SETTING) || defined(GET_TIME_FROM_PHONE)
 
-static CHSV dawnColor = CHSV(0, 0, 0);                                    // цвет "рассвета"
-static CHSV dawnColorMinus1 = CHSV(0, 0, 0);                              // для большей плавности назначаем каждый новый цвет только 1/10 всех диодов; каждая следующая 1/10 часть будет "оставать" на 1 шаг
-static CHSV dawnColorMinus2 = CHSV(0, 0, 0);
-static CHSV dawnColorMinus3 = CHSV(0, 0, 0);
-static CHSV dawnColorMinus4 = CHSV(0, 0, 0);
-static CHSV dawnColorMinus5 = CHSV(0, 0, 0);
+/* оптимизируем структуру данных и их обработчик
+  static CHSV dawnColor = CHSV(0, 0, 0);                                    // цвет "рассвета"
+  static CHSV dawnColorMinus1 = CHSV(0, 0, 0);                              // для большей плавности назначаем каждый новый цвет только 1/10 всех диодов; каждая следующая 1/10 часть будет "оставать" на 1 шаг
+  static CHSV dawnColorMinus2 = CHSV(0, 0, 0);
+  static CHSV dawnColorMinus3 = CHSV(0, 0, 0);
+  static CHSV dawnColorMinus4 = CHSV(0, 0, 0);
+  static CHSV dawnColorMinus5 = CHSV(0, 0, 0);
+  static CHSV dawnColor = CHSV(0, 0, 0);*/
+static CRGB dawnColor[6];
+
 static uint8_t dawnCounter = 0;                                           // счётчик первых 10 шагов будильника
 
 void timeTick()
@@ -61,7 +65,6 @@ void timeTick()
 #endif
           //    if (!timeSynched || millis() > ntpTimeLastSync + NTP_INTERVAL) // uint32_t ntpTimeLastSync
           //    {// если прошло более NTP_INTERVAL, значит, можно попытаться получить время с сервера точного времени один разок
-          //      phoneTimeLastSync = millis();
           if (timeClient.update()) {
 #ifdef WARNING_IF_NO_TIME
             noTimeClear();
@@ -85,8 +88,6 @@ void timeTick()
 
       //time_t currentLocalTime = localTimeZone.toLocal(timeClient.getEpochTime());
       time_t currentLocalTime = getCurrentLocalTime();
-      //jsonWrite(configSetup, "time", Get_Time(currentLocalTime));
-      //my_time = currentLocalTime;
 
       uint8_t thisDay = dayOfWeek(currentLocalTime);
       if (thisDay == 1) thisDay = 8;                                      // в библиотеке Time воскресенье - это 1; приводим к диапазону [0..6], где воскресенье - это 6
@@ -106,25 +107,38 @@ void timeTick()
           // величина рассвета 0-255
           int32_t dawnPosition = 255 * ((float)(thisFullTime - (alarms[thisDay].Time - pgm_read_byte(&dawnOffsets[dawnMode])) * 60) / (pgm_read_byte(&dawnOffsets[dawnMode]) * 60));
           dawnPosition = constrain(dawnPosition, 0, 255);
-          dawnColorMinus5 = dawnCounter > 4 ? dawnColorMinus4 : dawnColorMinus5;
-          dawnColorMinus4 = dawnCounter > 3 ? dawnColorMinus3 : dawnColorMinus4;
-          dawnColorMinus3 = dawnCounter > 2 ? dawnColorMinus2 : dawnColorMinus3;
-          dawnColorMinus2 = dawnCounter > 1 ? dawnColorMinus1 : dawnColorMinus2;
-          dawnColorMinus1 = dawnCounter > 0 ? dawnColor : dawnColorMinus1;
-          dawnColor = CHSV(map(dawnPosition, 0, 255, 10, 35),
+          /* оптимизируем структуру данных и их обработчик
+            dawnColorMinus5 = dawnCounter > 4 ? dawnColorMinus4 : dawnColorMinus5;
+            dawnColorMinus4 = dawnCounter > 3 ? dawnColorMinus3 : dawnColorMinus4;
+            dawnColorMinus3 = dawnCounter > 2 ? dawnColorMinus2 : dawnColorMinus3;
+            dawnColorMinus2 = dawnCounter > 1 ? dawnColorMinus1 : dawnColorMinus2;
+            dawnColorMinus1 = dawnCounter > 0 ? dawnColor : dawnColorMinus1;
+            dawnColor = CHSV(map(dawnPosition, 0, 255, 10, 35),
                            map(dawnPosition, 0, 255, 255, 170),
-                           map(dawnPosition, 0, 255, 2, DAWN_BRIGHT));
-          dawnCounter++;
+                           map(dawnPosition, 0, 255, 2, DAWN_BRIGHT));*/
+          for (uint8_t j = 5U; j > 0U; j--)
+            if (dawnCounter >= j)
+              dawnColor[j] = dawnColor[j - 1U];
+          dawnColor[0] = CHSV(map(dawnPosition, 0, 255, 10, 35),
+                              map(dawnPosition, 0, 255, 255, 170),
+                              map(dawnPosition, 0, 255, 2, DAWN_BRIGHT));
+
+          /* исправляем переполнение счётчика
+            dawnCounter++;*/
+          if (dawnCounter < 5U) dawnCounter++;
+
           // fill_solid(leds, NUM_LEDS, dawnColor);
+
           for (uint16_t i = 0U; i < NUM_LEDS; i++)
-          {
-            if (i % 6 == 0) leds[i] = dawnColor;                          // 1я 1/10 диодов: цвет текущего шага
-            if (i % 6 == 1) leds[i] = dawnColorMinus1;                    // 2я 1/10 диодов: -1 шаг
-            if (i % 6 == 2) leds[i] = dawnColorMinus2;                    // 3я 1/10 диодов: -2 шага
-            if (i % 6 == 3) leds[i] = dawnColorMinus3;                    // 3я 1/10 диодов: -3 шага
-            if (i % 6 == 4) leds[i] = dawnColorMinus4;                    // 3я 1/10 диодов: -4 шага
-            if (i % 6 == 5) leds[i] = dawnColorMinus5;                    // 3я 1/10 диодов: -5 шагов
-          }
+            /*{ оптимизируем цикл
+              if (i % 6 == 0) leds[i] = dawnColor;                          // 1я 1/10 диодов: цвет текущего шага
+              if (i % 6 == 1) leds[i] = dawnColorMinus1;                    // 2я 1/10 диодов: -1 шаг
+              if (i % 6 == 2) leds[i] = dawnColorMinus2;                    // 3я 1/10 диодов: -2 шага
+              if (i % 6 == 3) leds[i] = dawnColorMinus3;                    // 3я 1/10 диодов: -3 шага
+              if (i % 6 == 4) leds[i] = dawnColorMinus4;                    // 3я 1/10 диодов: -4 шага
+              if (i % 6 == 5) leds[i] = dawnColorMinus5;                    // 3я 1/10 диодов: -5 шагов
+              }*/
+            leds[i] = dawnColor[i % 6U];
           FastLED.setBrightness(255);
           delay(1);
           FastLED.show();
@@ -154,12 +168,17 @@ void timeTick()
           changePower();                                                  // выключение матрицы или установка яркости текущего эффекта в засисимости от того, была ли включена лампа до срабатывания будильника
         }
         manualOff = false;
-        dawnColorMinus1 = CHSV(0, 0, 0);
-        dawnColorMinus2 = CHSV(0, 0, 0);
-        dawnColorMinus3 = CHSV(0, 0, 0);
-        dawnColorMinus4 = CHSV(0, 0, 0);
-        dawnColorMinus5 = CHSV(0, 0, 0);
+        /* оптимизируем структуру данных и их обработчик
+          dawnColorMinus1 = CHSV(0, 0, 0);
+          dawnColorMinus2 = CHSV(0, 0, 0);
+          dawnColorMinus3 = CHSV(0, 0, 0);
+          dawnColorMinus4 = CHSV(0, 0, 0);
+          dawnColorMinus5 = CHSV(0, 0, 0);*/
+        for (uint8_t j = 0U; j < 6U; j++)
+          dawnColor[j] = 0;
+
         dawnCounter = 0;
+
 
 #if defined(ALARM_PIN) && defined(ALARM_LEVEL)                    // установка сигнала в пин, управляющий будильником
         digitalWrite(ALARM_PIN, !ALARM_LEVEL);
@@ -186,8 +205,6 @@ void resolveNtpServerAddress(bool &ntpServerAddressResolved)              // ф�
   if (err != 1 || ntpServerIp[0] == 0 || ntpServerIp == IPAddress(255U, 255U, 255U, 255U))
   {
 #ifdef GENERAL_DEBUG
-    //LOG.print(F("Статус Error: "));
-    //LOG.println(err);
     LOG.print(F("IP адрес NTP: "));
     LOG.println(ntpServerIp);
 #endif
@@ -219,9 +236,25 @@ void getFormattedTime(char *buf)
 time_t getCurrentLocalTime()
 {
 #if defined(USE_NTP) || defined(USE_MANUAL_TIME_SETTING) || defined(GET_TIME_FROM_PHONE)
+#if defined(USE_MANUAL_TIME_SETTING) || defined(GET_TIME_FROM_PHONE)
+  static uint32_t milliscorrector;
+#endif
+
   if (timeSynched)
   {
 #if defined(USE_NTP) && defined(USE_MANUAL_TIME_SETTING) || defined(USE_NTP) && defined(GET_TIME_FROM_PHONE)
+    if (milliscorrector > millis()
+#ifdef GET_TIME_FROM_PHONE
+        && manualTimeShift + millis() / 1000UL < phoneTimeLastSync
+#endif
+       ) {
+      manualTimeShift += 4294967; // защищаем время от переполнения millis()
+#ifdef GET_TIME_FROM_PHONE
+      phoneTimeLastSync += 4294967; // а это, чтобы через 49 дней всё не заглючило
+#endif
+    }
+    milliscorrector = millis();
+
     if (ntpServerAddressResolved)
       return localTimeZone.toLocal(timeClient.getEpochTime());
     else
@@ -229,6 +262,17 @@ time_t getCurrentLocalTime()
 #endif
 
 #if !defined(USE_NTP) && defined(USE_MANUAL_TIME_SETTING) || !defined(USE_NTP) && defined(GET_TIME_FROM_PHONE)
+    if (milliscorrector > millis()
+#ifdef GET_TIME_FROM_PHONE
+        && manualTimeShift + millis() / 1000UL < phoneTimeLastSync
+#endif
+       ) {
+      manualTimeShift += 4294967; // защищаем время от переполнения millis()
+#ifdef GET_TIME_FROM_PHONE
+      phoneTimeLastSync += 4294967; // а это, чтобы через 49 дней всё не заглючило
+#endif
+    }
+    milliscorrector = millis();
     return millis() / 1000UL + manualTimeShift;
 #endif
 
